8e73252383da10ee180fe4708ab88c4a85916314
ChabbakiAymane <aymane.chabbaki@studenti.unitn.it>
2025-02-17 17:45:36 +0100

    Commetti le modifiche prima di applicare lo stash

--- obj/Debug/net9.0/Gamestore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Gamestore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffc4fc3847cd96467caa57bd930863d4224aabbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01fd44be6baa09df0134cd25959b73147ab5448")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
