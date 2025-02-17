4fa2709e94d3d1de11bb98bf7ef6180b8c3319f2
ChabbakiAymane <aymane.chabbaki@studenti.unitn.it>
2025-02-17 17:40:40 +0100

    Risoluzione conflitti

--- obj/Debug/net9.0/Gamestore.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Gamestore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0464d246f4d4993efad7dd31e2e887d3c937863")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0644a7c533c1b6aba59e760a293f2c3f80e738")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gamestore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
